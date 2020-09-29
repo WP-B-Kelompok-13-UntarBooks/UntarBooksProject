df19663c50311ef2bbef76d9d74bfc69087b507e
yoshidaporcher <71253762+yoshidaporcher@users.noreply.github.com>
2020-09-29 16:31:03 +0700

    Add files via upload

---
+++ book-item.js
@@ -0,0 +1,25 @@
+class BookItem extends HTMLElement {
+  set book (book) {
+    this._book = book
+    this.render()
+  }
+
+  render () {
+    this.innerHTML = `
+        <div class="card">
+          <div class="card-body bg-secondary text-white border rounded">
+            <div class="row">
+              <div class="col border">
+                <img src="${this._book.volumeInfo.imageLinks.thumbnail}" class="img-fluid">
+              </div>
+              <div class="col border">
+                <h4>${this._book.volumeInfo.title}</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        `
+  }
+}
+
+customElements.define('book-item', BookItem)
