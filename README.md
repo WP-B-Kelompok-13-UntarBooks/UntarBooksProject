d6174ed03ddf75496edaa62897fc57e77f094b73
Teddy Lioner <teddylioner@gmail.com>
2020-12-13 22:50:49 +0700

    ejs+xpress final

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# UntarBooksProject
+
+ Now available on: https://youthful-jennings-dae795.netlify.app/
+
+How To Open
+1. Install Node and NPM 12 lts from https://nodejs.org/
+2. Open the directory using VSCODE
+3. Open the terminal
+4. Run this command to the terminal "npm install"
+5. Run this command to open live server "npm run start-dev"
+6. Or this command to build the html and js "npm run build" (files located at /.dist)
+
+This project uses webpack, npm, and googlebooks API.
+This project aim to show how to fetch data and do search from an API.
+
+
+List To-Do :
+1. Add mechanic to pagination
+2. Add Routing
---
+++ routes/detail.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+
+router.get('/:Id', 
+    async(req, res) => {
+      var Id = req.params.Id;
+      console.log("Masuk detail: " + Id);
+      _title_page = "Novel Books";
+
+      fetch(`https://www.googleapis.com/books/v1/volumes/${Id}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) {
+          Promise.reject(`${Id} is not found`)
+        }
+        else{
+          var renderbook = data;
+          res.render('pages/detail', { book:renderbook });
+        }
+      })
+
+    }
+);
+module.exports = router;
